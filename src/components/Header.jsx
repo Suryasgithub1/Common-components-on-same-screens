5d3e57b31097bac14e41325526ec890a2ebf4f1a
Suryasgithub1 <dsurya27299@gmail.com>
2025-05-10 19:32:45 +0530

    Fixed error in Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import Tab1 from './Tab1';
 import Tab2 from './Tab2';
 import Tab3 from './Tab3';
 import Tab4 from './Tab4';
-import Tab5 from "./tab5"
+import Tab5 from "./Tab5"
 
 const Header = () => {
     const [showComponent, setShowComponent] = useState(null); 
